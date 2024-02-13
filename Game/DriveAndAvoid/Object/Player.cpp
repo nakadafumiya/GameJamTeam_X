825944a02b253afe4ead362c35d8ca6b83c81bc1
初心者狩りのゾロ <106945336+nakadafumiya@users.noreply.github.com>
2024-02-13 13:46:15 +0900

    いらない画像消去

--- Game/DriveAndAvoid/Object/Player.cpp
+++ Game/DriveAndAvoid/Object/Player.cpp
@@ -4,7 +4,7 @@
 
 Player::Player() : is_active(false), image(NULL), location(0.0f), box_size(0.0f),
 angle(0.0f),
-speed(0.0f), hp(0.0f), fuel(0.0f), barrier_count(0),
+speed(0.0f), hp(0.0f),  barrier_count(0),
 barrier(nullptr)
 {
 
